05a8ffa10ecce73cc1e7ab830b395dbfd9ab8710
Mariángel <126704306+MariangelQuiceno@users.noreply.github.com>
2024-02-20 14:11:43 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { HeaderPart } from "./components/Pages/HeaderPart/HeaderPart"
+import { MainPart } from "./components/Pages/MainPart/MainPart"
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="Father">
+    <HeaderPart/>
+    <MainPart/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Pages/HeaderPart/HeaderPart.jsx
@@ -0,0 +1,14 @@
+import { Header } from '../../Layouts/Header/Header'
+import { Item } from '../../headerComponents/Item/Item';
+import './headerPartCss.css'
+
+
+export const HeaderPart = () => {
+    return (
+      <div className="header-container"> 
+         <Header>
+         <p>dertrte</p>
+         </Header>
+      </div>
+    );
+  };
